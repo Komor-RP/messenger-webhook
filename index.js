abc8aa4be3bac95000036c56f714a8d523997c98
Fan Huang <fan.huang.33@gmail.com>
2018-08-12 10:55:40 -0400

    added nodejs request module

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const
   express = require('express'),
   bodyParser = require('body-parser'),
+  request = require('request'),
   app = express().use(bodyParser.json()); // creates express http server
+
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
