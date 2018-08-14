66811e0059b9c307852d09a21aa436c1b1a022b6
Fan Huang <fan.huang.33@gmail.com>
2018-08-14 00:38:04 -0400

    a

--- index.js
+++ index.js
@@ -137,10 +137,13 @@ function handleMessage(sender_psid, received_message) {
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
   let response;
+  console.log("received_postback:" + received_postback);
 
   // Get the payload for the postback
   let payload = received_postback.payload;
-
+  if (payload.title = "Get Started") {
+    response = payload.postback;
+  }
   // Set the response based on the postback payload
   if (payload === 'yes') {
     response = { "text": "Thanks!" }
