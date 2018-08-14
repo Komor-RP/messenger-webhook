aa69fda837d5950236b66cf0c9bd352cc238d9c2
Fan Huang <fan.huang.33@gmail.com>
2018-08-14 19:36:44 -0400

    a

--- index.js
+++ index.js
@@ -146,6 +146,7 @@ function receivedPostback(event) {
     var recipientID = event.recipient.id;
     var timeOfPostback = event.timestamp;
     var payload = event.postback.payload;
+    console.log("switch: " + payload);
 
     console.log("Received postback for user %d and page %d with payload '%s' " +
         "at %d", senderID, recipientID, payload, timeOfPostback);
